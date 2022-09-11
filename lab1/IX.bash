bab1bb71c97c78196d505ceb5e15d44f6bbe8d3e
pypkaed <89941898+pypkaed@users.noreply.github.com>
2022-09-11 15:25:20 +0300

    Add files via upload

--- lab1/IX.bash
+++ lab1/IX.bash
@@ -2,4 +2,4 @@
 
 # 2>/dev/null -> redirect error messages to /dev/null
 # wc -l -> pring the newline counts
-find /var/log -type f -name "*.log" 2>/dev/null | wc -l
+find /var/log -type f -name "*.log" 2>/dev/null -exec cat {} \; | wc -l
